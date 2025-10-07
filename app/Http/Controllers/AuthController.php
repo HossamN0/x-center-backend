9fb7ccc241b8955e3e60794a1fd4f5b2e51c8e23
HossamN0 <bshossam@gmail.com>
2025-10-07 04:43:00 +0300

    database ready - auth (with JWT - access & refresh tokens)

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required',
+            'password' => 'required',
+            'role' => ['optional', Rule::in(['admin', 'instructor', 'student'])],
+        ]);
+
+        $role = $validated['role'] ?? 'student';
+
+        User::create([
+            'first_name' => $validated['first_name'],
+            'last_name' => $validated['last_name'],
+            'email' => $validated['email'],
+            'phone' => $validated['phone'],
+            'password' => Hash::make($validated['password']),
+            'role' => $role,
+            'status' => $role === 'instructor' ? 'inactive' : 'active',
+        ]);
+
+        return response()->json([
+            'message' => 'User registered successfully'
+        ], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!$token = JWTAuth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $refresh_token = JWTAuth::claims([
+            'token_type' => 'refresh',
+        ])->fromUser(JWTAuth::user());
+
+        return response()->json([
+            'message' => 'Login successful',
+            'user' => JWTAuth::user(),
+            'access_token' => $token,
+            'refresh_token' => $refresh_token,
+        ], 200);
+    }
+
+    public function refresh(Request $request)
+    {
+        $request->validate([
+            'refresh_token' => 'required|string',
+        ]);
+        try {
+            $refresh_token = $request->refresh_token;
+            $payload = JWTAuth::setToken($refresh_token)->getPayload();
+
+            if ($payload->get('token_type') !== 'refresh') {
+                return response()->json([
+                    'message' => 'Invalid refresh token'
+                ], 401);
+            }
+
+            $user = JWTAuth::setToken($refresh_token)->toUser();
+            JWTAuth::invalidate($refresh_token);
+            $access_token = JWTAuth::fromUser($user);
+            $refresh_token = JWTAuth::claims([
+                'token_type' => 'refresh',
+            ])->fromUser($user);
+
+            return response()->json([
+                'message' => 'Token refreshed successfully',
+                'access_token' => $access_token,
+                'refresh_token' => $refresh_token
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage()
+            ], 401);
+        }
+    }
+}
---
+++ database/migrations/2025_10_07_002012_create_course_exams_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_exams', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('chapter_id')->constrained('course_chapters')->onDelete('cascade');
+            $table->enum('status', ['not_started', 'inprogress', 'completed'])->default('not_started');
+            $table->float('degree')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_exams');
+    }
+};
